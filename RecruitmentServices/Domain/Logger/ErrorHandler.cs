eec34005571ea5f72dbc1ef2fb94c02b91c36926
Zola Matshoba <zola.matshoba@gmail.com>
2020-02-08 12:10:25 +0200

    Domain Error Handler
    added error handler for domain project

---
+++ RecruitmentServices/Domain/Logger/ErrorHandler.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.IO;
+
+namespace Domain
+{
+    public static class ErrorHandler
+    {
+        public static void ErrorLogger(this Exception exception)
+        {
+            StreamWriter writer = new StreamWriter("");
+            writer.WriteLine(exception.Message + DateTime.Today.ToString("dd/MM/yyyy"));
+            writer.Close();
+        }
+        public static void ErrorStack (this Exception exception)
+        {
+            StreamWriter writer = new StreamWriter("");
+            writer.WriteLine(exception.StackTrace + DateTime.Today.ToString("dd/MM/yyyy"));
+            writer.Close();
+        }
+    }
+}
